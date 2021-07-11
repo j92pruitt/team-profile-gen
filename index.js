18b8b113c532a390997031ff10a91ac9ebdf8264
j92pruitt <j92pruitt@gmail.com>
2021-07-11 13:41:31 -0700

    Finished logic for general program flow and user queries.

--- index.js
+++ index.js
@@ -32,10 +32,95 @@ selectorPrompt = new Prompt( [
     }
 ] )
 
-managerPrompt.query()
-    .then( answer => {
-        console.log(answer)
-        
-        selectorPrompt.query()
-            .then( answer => console.log(answer))
-    } )
+engineerPrompt = new Prompt( [
+    {
+        type : 'input',
+        message : 'What is their name?',
+        name : 'name'
+    },
+    {
+        type : 'input',
+        message : 'What is their employee id number?',
+        name : 'id'
+    },
+    {
+        type : 'input',
+        message : 'What is their email?',
+        name : 'email'
+    },
+    {
+        type : 'input',
+        message : 'What is their GitHub username?',
+        name : 'username'
+    }
+] )
+
+internPrompt = new Prompt( [
+    {
+        type : 'input',
+        message : 'What is their name?',
+        name : 'name'
+    },
+    {
+        type : 'input',
+        message : 'What is their employee id number?',
+        name : 'id'
+    },
+    {
+        type : 'input',
+        message : 'What is their email?',
+        name : 'email'
+    },
+    {
+        type : 'input',
+        message : 'What school do they attend?',
+        name : 'school'
+    }
+] )
+
+function init() {
+    createTeamMember('Manager')
+}
+
+function createTeamMember(job) {
+    if (job === 'Engineer') {
+        engineerPrompt.query()
+            .then(
+                // Placeholder for engineer creation
+                // Placeholder for adding engineer to team
+                runSelection
+            );
+    } else if (job === 'Intern') {
+        internPrompt.query()
+            .then(
+                // Placeholder for intern creation
+                // Placeholder for adding intern to team
+                runSelection
+            );
+    } else if (job === 'Manager') {
+        managerPrompt.query()
+            .then(
+                // Placeholder for manager creation
+                // Placeholder for adding manager to team
+                runSelection
+            );
+    } else {
+        Error(`Unexpected job selection: ${job}`)
+    }
+}
+
+function runSelection() {
+    selectorPrompt.query()
+        .then(
+            ({choice : choice}) => {
+                console.log(choice)
+                if (choice === 'Finished adding team members') {
+                    return console.log('Generating HTML');
+                 } else {
+                    return createTeamMember(choice);
+                 }
+            }
+        );
+}
+
+init();
