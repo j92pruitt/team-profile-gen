f55c2e5b41ea8a1ce7c5b7f27e779531c6b57fa7
j92pruitt <j92pruitt@gmail.com>
2021-07-11 11:57:18 -0700

    Added prompt object and related tests.

---
+++ tests/prompts.test.js
@@ -0,0 +1,39 @@
+const { describe, it, expect} = require('@jest/globals');
+const Prompt = require('../lib/prompt');
+const inquirer = require('inquirer');
+const { ModuleMocker } = require('jest-mock');
+
+jest.mock('inquirer')
+
+describe('Prompt', () => {
+    describe('Initialization', () => {
+
+        it('should create an object with "questions" property set to "questions" argument provided when called with "new" keyword.', () => {
+
+            const questions = ['Q1','Q2'];
+
+            const obj = new Prompt(questions)
+
+            expect(obj).toHaveProperty('questions', questions)
+        });
+    });
+
+    describe('Query', () => {
+
+        it('should call "inquirer.prompt" with the "questions" as input', () => {
+
+            const questions = ['Q1','Q2'];
+
+            const obj = new Prompt(questions)
+
+            inquirer.prompt.mockReturnValue(
+                new Promise(function(resolve) {
+                    resolve({Q1 : "A1", Q2: "A2"})
+                })
+            );
+
+            expect(obj.query()).resolves.toEqual({Q1 : "A1", Q2: "A2"})
+            expect(inquirer.prompt).lastCalledWith(questions)
+        })
+    })
+})
