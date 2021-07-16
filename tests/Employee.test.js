c2d53dc0007401b90dae22a05c6fad251a1e24ff
j92pruitt <j92pruitt@gmail.com>
2021-07-15 18:25:11 -0700

    Added Manager class and tests. All tests passed.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -5,21 +5,21 @@ describe('Employee', () => {
 
     describe('Initialization', () => {
 
-        it('should should create an object with "name" property set to "name" parameter.', () => {
+        it('should create an object with "name" property set to "name" parameter.', () => {
 
             const obj = new Employee('test name', '0', 'email@test.com');
 
             expect(obj).toHaveProperty('name', 'test name');
         })
 
-        it('should should create an object with "id" property set to "id" parameter.', () => {
+        it('should create an object with "id" property set to "id" parameter.', () => {
 
             const obj = new Employee('test name', '0', 'email@test.com');
 
             expect(obj).toHaveProperty('id', '0');
         })
 
-        it('should should create an object with "email" property set to "email" parameter.', () => {
+        it('should create an object with "email" property set to "email" parameter.', () => {
 
             const obj = new Employee('test name', '0', 'email@test.com');
 
---
+++ tests/Manager.test.js
@@ -0,0 +1,33 @@
+const { describe, it, expect} = require('@jest/globals')
+const Employee = require('../lib/Employee');
+const Manager = require('../lib/Manager');
+
+describe('Manager', () => {
+
+    describe('Initialization', () => {
+
+        it('should extend the Empolyee class.', () => {
+
+            const obj = new Manager('Manny', '1', 'manny@test.com', '21');
+
+            expect(obj).toBeInstanceOf(Employee);
+        })
+
+        it('should create an object with "office" property set to "office" parameter.', () => {
+
+            const obj = new Manager('Manny', '1', 'manny@test.com', '21');
+
+            expect(obj).toHaveProperty('office', '21');
+        })
+    })
+
+    describe('getRole', () => {
+
+        it('should return "Manager".', () => {
+
+            const obj = new Manager('Manny', '1', 'manny@test.com', '21');
+
+            expect( obj.getRole() ).toBe('Manager')
+        })
+    })
+})
